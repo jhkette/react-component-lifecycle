a0c3429674e7db2c4dad8b0a6247998784be76b6
khenryjoe <joseph.ketterer@gmail.com>
2019-04-15 17:40:37 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,97 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class LifecycleDemo
+extends React.Component {
+  state = { counter: 0 };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  constructor(props) {
+    super(props);
+    console.log("Constructing...");
+    console.log(
+      "State already set to:",
+      this.state
+    );
+  }
+
+  componentWillMount() {
+    console.log(
+      "About to mount..."
+    );
+  }
+
+  componentDidMount() {
+    console.log("Mounted.");
+  }
+
+  componentWillReceiveProps(
+    nextProps
+  ) {
+    console.log(
+      "Current props:",
+      this.props
+    );
+    console.log(
+      "Next props:",
+      nextProps
+    );
+  }
+
+  shouldComponentUpdate(
+    nextProps,
+    nextState
+  ) {
+    console.log(
+      "Deciding to update"
+    );
+    return true;
+  }
+
+  componentWillUpdate(
+    nextProps,
+    nextState
+  ) {
+    console.log(
+      "About to update..."
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("Updated.");
+  }
+
+  componentWillUnmount() {
+    console.log(
+      "Goodbye cruel world."
+    );
+  }
+
+  handleClick = () => {
+    this.setState({
+      counter:
+        this.state.counter + 1
+    });
+  };
+
+  render() {
+    console.log("Rendering...");
+    return (
+      <div>
+        <span>
+          Counter:{" "}
+          {this.state.counter}
+        </span>
+        <button
+          onClick={this.handleClick}
+        >
+          Click to increment
+        </button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <LifecycleDemo />,
+  document.querySelector("#root")
+);
